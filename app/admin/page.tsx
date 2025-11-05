81245aa02f5757b082ce8be91605295015a1d589
umarbek <kinddevs2024@gmail.com>
2025-11-05 14:55:07 +0500

    a

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, JSX } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 
